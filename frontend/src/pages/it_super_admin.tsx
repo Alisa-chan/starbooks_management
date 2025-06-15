39dff5498ed9905a501e8ca280cb496586c327e4
Alisa-chan <elizercenonlibatiqeu@gmail.com>
2025-06-16 01:01:40 +0800

    new file

---
+++ frontend/src/pages/it_super_admin.tsx
@@ -0,0 +1,9 @@
+function ITSuperAdmin() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to the IT Super Admin Dashboard!</h1>
+    </div>
+  );
+}
+
+export default ITSuperAdmin;
