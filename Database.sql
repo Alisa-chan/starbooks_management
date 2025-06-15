58645f3c7db1414796c23b45a1ca09a59e6257a3
Alisa-chan <elizercenonlibatiqeu@gmail.com>
2025-06-16 00:45:36 +0800

    Added admin it users

--- Database.sql
+++ Database.sql
@@ -1,13 +1,30 @@
 -- DATABASE Schema for Dost
 CREATE DATABASE dost;
 
--- Set up uuid "uuid-ossp"; 
+
 CREATE TABLE users (
     user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL
+    user_email VARCHAR(255) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL,
+    user_role VARCHAR(50) NOT NULL DEFAULT 'user' -- 'admin' or 'it' 
 );
 
 
--- Insert a test user
-INSERT INTO users (user_email, user_password) VALUES ('testing@gmail.com', 'Test123');
+-- Alterd the database 
+ALTER TABLE users ADD COLUMN user_role VARCHAR(50) NOT NULL DEFAULT 'user';
+
+
+-- Normal user
+INSERT INTO users (user_email, user_password, user_role)
+VALUES ('testing@gmail.com', 'Test123', 'user');
+
+-- Admin
+INSERT INTO users (user_email, user_password, user_role)
+VALUES ('admin@gmail.com', 'admin123', 'admin');
+
+-- IT Super Admin
+INSERT INTO users (user_email, user_password, user_role)
+VALUES ('it@gmail.com', 'it123', 'it');
+
+
+--Still not encrypted just testing yet
